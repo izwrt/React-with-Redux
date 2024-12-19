d57dc76405d470cc93d2e0a0a0f4eef00b5aeaaa
izwrt <ishwart466@gmail.com>
2024-12-19 20:25:12 +0530

    created function to pass inside dispatch

--- Redux-banking-app/src/store.js
+++ Redux-banking-app/src/store.js
@@ -1,4 +1,4 @@
-import {createStore} from 'redux'
+import { createStore } from 'redux';
 
 /* eslint-disable no-duplicate-case */
 const initialState = {
@@ -34,14 +34,45 @@ function reducer(state = initialState, action) {
 
 const store = createStore(reducer);
 
-store.dispatch({type: "account/deposite", payload:1000});
-console.log(store.getState());
+// store.dispatch({type: "account/deposite", payload:1000});
+// console.log(store.getState());
 
-store.dispatch({type: 'account/withdraw', payload:500});
-console.log(store.getState());
+// store.dispatch({type: 'account/withdraw', payload:500});
+// console.log(store.getState());
 
-store.dispatch({type: "account/requestLoan", payload: { amount:1000, purpose: "Buy a car"}})
-console.log(store.getState());
+// store.dispatch({type: "account/requestLoan", payload: { amount:1000, purpose: "Buy a car"}})
+// console.log(store.getState());
 
-store.dispatch({type: "account/payLoan", payload:100})
+// store.dispatch({type: "account/payLoan", payload:100})
+// console.log(store.getState());
+
+function deposite(amount) {
+    return {
+        type: "account/deposite", payload:amount
+    }
+}
+
+function withdraw(amount) {
+    return {
+        type: 'account/withdraw', payload:amount
+    }
+}
+
+function requestLoan (amount, purpose) {
+    return {
+        type: "account/requestLoan", payload: { amount:amount, purpose: purpose}
+    }
+}
+
+function payLoan ( amount ) {
+    return {
+        type: "account/payLoan", payload:amount
+    }
+}
+
+store.dispatch(deposite(1000));
+store.dispatch(withdraw(1000));
+store.dispatch(requestLoan(1000));
+store.dispatch(payLoan(100));
+
 console.log(store.getState());
