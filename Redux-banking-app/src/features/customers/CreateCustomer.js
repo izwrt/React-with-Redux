290aafd0232f9771d0cf53b1b76757b9c97bbcb0
izwrt <ishwart466@gmail.com>
2024-12-26 17:40:55 +0530

    redux with ui

--- Redux-banking-app/src/features/customers/CreateCustomer.js
+++ Redux-banking-app/src/features/customers/CreateCustomer.js
@@ -1,10 +1,20 @@
 import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { createCustomer } from "./cusomerSlice";
 
 function Customer() {
   const [fullName, setFullName] = useState("");
   const [nationalId, setNationalId] = useState("");
 
-  function handleClick() {}
+  const dispatch = useDispatch();
+
+  function handleClick(e) {
+    e.preventDefault();
+    if(!fullName || !nationalId){
+      return;
+    }
+    dispatch(createCustomer(fullName,nationalId));
+  }
 
   return (
     <div>
